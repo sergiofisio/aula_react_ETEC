0f39a6d5d366b57caf23acd1dbfb3a324aad5929
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-04-27 14:29:50 -0300

    first commit

--- src/componets/header/button/index.jsx
+++
@@ -1,9 +0,0 @@
-import "./button.css";
-
-export default function Button({ icon, className }) {
-  return (
-    <button className={className}>
-      <img src={icon} alt="Icone de login" />
-    </button>
-  );
-}
--- src/componets/header/link/index.jsx
+++
@@ -1,6 +0,0 @@
-import { Link } from "react-router-dom";
-import "./link.css";
-
-export default function Menu({ link, text }) {
-  return <Link to={link}>{text}</Link>;
-}
