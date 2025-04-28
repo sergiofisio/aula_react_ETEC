62d9875c35f760841b7441cb494ad503acbe87be
BEaew0 <beatrizsouzasscoli@gmail.com>
2025-04-27 23:19:32 -0300

    Mudanças no cardapio

--- src/pages/cardapio/index.jsx
+++ src/pages/cardapio/index.jsx
@@ -1,10 +1,13 @@
 
-
+import "./cardapio.css";
 
 export default function Cardapio() {
   return (
-    <div>
-      <h2>Cardapio</h2>
+    <div  id="main_cardapio" className="cardapio">
+      <h2>Menu principal</h2>
+      
+      
+
 
     </div>
   );
