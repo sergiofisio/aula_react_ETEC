03460b9ef77cea0e154e6b764765f0a59ca4ef00
BEaew0 <beatrizsouzasscoli@gmail.com>
2025-04-29 10:51:19 -0300

    importando imagens

--- src/pages/cardapio/index.jsx
+++ src/pages/cardapio/index.jsx
@@ -1,7 +1,30 @@
+
+import "./cardapio.css";
+
+import Imagem from "../../components/footer/imagem";
+
+import bolos  from "../../assets/img/bolos.svg";
+import bebidas from "./../assets/img/bebidas.svg";
+import especialidades from"../../assets/img/especialidades.svg"
+import recomendacoes from "../../assets/img/buffet1.svg"
+
+
+const imagens = [
+  {
+    
+  },
+  {  },
+  {  },
+];
+
+
+
 export default function Cardapio() {
   return (
-    <div>
-      <h2>Cardapio</h2>
+    <div id="main_cardapio">
+      <h2>Menu principal</h2>
+
+      <Imagem/>
     </div>
   );
 }
