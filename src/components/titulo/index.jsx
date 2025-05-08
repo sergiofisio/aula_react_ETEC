2243dd9e4238f8ce72099d6abea1817ed958146e
Sergio Bastos <sergiobastosfisio@gmail.com>
2025-05-08 09:36:09 -0300

    pages

--- src/components/titulo/index.jsx
+++ src/components/titulo/index.jsx
@@ -1,9 +1,5 @@
 import "./titulo.css";
 
 export default function Titulo({ texto, style }) {
-    return <h1 className={style}>{texto}</h1>;
-}import "./titulo.css";
-
-export default function Titulo({ texto, style }) {
   return <h1 className={style}>{texto}</h1>;
 }
